04173ebdbd399d4e2cd546c449cd78a57f768360
Albert S. Kim <enphysoft@users.noreply.github.com>
2025-04-17 10:04:10 -1000

    Update twoCarCollisionAnalysis.m

--- twoCarCollisionAnalysis.m
+++ twoCarCollisionAnalysis.m
@@ -12,12 +12,12 @@ wai    =  0.0; wbi    =  0.0;  % angular velocities
 rcoef  = 0.2 ;                 % restitution coefficient "e" 
 theta_degree = 20           ;  % collision angle made using initial velocity
 %=================
-
 % geometrical setup
 tdir = [1,0] ;  % Tangential direction
 ndir = [0,1] ;  % Normal direction 
 hdia_a = sqrt(hlen_a^2 + hwid_a^2 ) ;  % distance from the CM to contact point of car A
 hdia_b = sqrt(hlen_b^2 + hwid_b^2 ) ;  % distance from the CM to contact point of car B
+
 % For car A
 theta_a = theta_degree  * pi/180        ;
 angle_a = theta_a + atan(hwid_a/hlen_a) ;
@@ -27,6 +27,7 @@ uai     = [ uat , uan]                  ;
 rvec_at =  hdia_a*cos(angle_a)          ;
 rvec_an =  hdia_a*sin(angle_a)          ;
 rvec_a  = [rvec_at,  rvec_an ]          ;
+
 % For car B
 theta_b = -0.0 * pi/180         ;
 ubt     = ubmag*cos(theta_b)    ;
@@ -57,7 +58,6 @@ vbt = ubt ;
 vaf = [vat, van ];
 vbf = [vbt, vbn ];
 
-
 % display
 fprintf('\n=== Pair-Collision Simulation === ')
 fprintf('\n=== Parameters === \n')
@@ -72,4 +72,4 @@ fprintf('Pre-collision  velocity of "B" : (uat, uan, waz ) = (%+.3f, %+.3f, %+.3
 fprintf('Post-collision velocity of "A" : (vat, van, waf ) = (%+.3f, %+.3f, %+.3f)\n', vat, van, waf); 
 fprintf('Post-collision velocity of "B" : (vat, van, waf ) = (%+.3f, %+.3f, %+.3f)\n', vbt, vbn, wbf);
 fprintf('\n');
-% '%+.3f'
+ 
